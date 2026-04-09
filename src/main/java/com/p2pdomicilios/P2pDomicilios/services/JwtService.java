51ab3b2fce2c94ea9ab06ee327882ec5c6942ad7
Juan Diego Contreras Garcia <juandiegocg@ufps.edu.co>
2026-04-09 11:00:37 -0500

    cambios revisables

--- src/main/java/com/p2pdomicilios/P2pDomicilios/services/JwtService.java
+++ src/main/java/com/p2pdomicilios/P2pDomicilios/services/JwtService.java
@@ -8,7 +8,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -70,16 +70,15 @@ public class JwtService {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
+    return Jwts.parser()
+            .verifyWith(getSignInKey()) // 🌟 Ahora sí reconocerá la SecretKey
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+}
 
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+    private SecretKey getSignInKey() { // 🌟 Cambiado de Key a SecretKey
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+}
 }
